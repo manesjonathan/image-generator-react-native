5d035fecad3e56cfde56fc900dd5e9221175658f
Jonathan Manes <manesjona@gmail.com>
2023-05-16 16:43:44 +0200

    styling component

--- screens/components/GoogleLogin.tsx
+++ screens/components/GoogleLogin.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
-import {Button, Platform} from "react-native";
+import {Button, Platform, View} from "react-native";
 import Cookies from "js-cookie";
 import * as SecureStore from "expo-secure-store";
 import {googleSignIn} from "../../utils/api";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-import {WEB_CLIENT_ID} from "../../utils/config";
+import {ANDROID_CLIENT_ID, WEB_CLIENT_ID} from "../../utils/config";
 
 WebBrowser.maybeCompleteAuthSession();
 type GoogleSigninProps = {
@@ -13,7 +13,7 @@ type GoogleSigninProps = {
 };
 const GoogleLogin = ({navigation}: GoogleSigninProps) => {
     const [request, response, promptAsync] = Google.useAuthRequest({
-        androidClientId: WEB_CLIENT_ID,
+        androidClientId: ANDROID_CLIENT_ID,
         webClientId: WEB_CLIENT_ID,
         iosClientId: "GOOGLE_GUID.apps.googleusercontent.com",
 
@@ -36,29 +36,31 @@ const GoogleLogin = ({navigation}: GoogleSigninProps) => {
     };
 
     return (
-        <Button
-            title="Sign in with Google"
-            disabled={!request}
-            onPress={() => {
-                promptAsync().then((res) => {
-                    if (res?.type === "success") {
-                        if (res.authentication) {
-                            getUserInfo(res.authentication.accessToken).then((res) => {
-                                googleSignIn(res).then(async res => {
-                                        if (Platform.OS === 'web') {
-                                            Cookies.set('JWT', res);
-                                        } else {
-                                            await SecureStore.setItemAsync('JWT', JSON.stringify(res));
+        <View style={{marginTop: 40}}>
+            <Button
+                title="Sign in with Google"
+                disabled={!request}
+                onPress={() => {
+                    promptAsync().then((res) => {
+                        if (res?.type === "success") {
+                            if (res.authentication) {
+                                getUserInfo(res.authentication.accessToken).then((res) => {
+                                    googleSignIn(res).then(async res => {
+                                            if (Platform.OS === 'web') {
+                                                Cookies.set('JWT', res);
+                                            } else {
+                                                await SecureStore.setItemAsync('JWT', JSON.stringify(res));
+                                            }
+                                            navigation.replace('App');
                                         }
-                                        navigation.replace('App');
-                                    }
-                                );
-                            });
+                                    );
+                                });
+                            }
                         }
-                    }
-                });
-            }}
-        />
+                    });
+                }}
+            />
+        </View>
     )
 }
 export default GoogleLogin;
