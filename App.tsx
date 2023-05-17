6302fe187404ddb0d87c659686b26f162405ee14
Jonathan Manes <manesjona@gmail.com>
2023-05-17 16:47:19 +0200

    Stripe config and display payment sheet

--- App.tsx
+++ App.tsx
@@ -6,33 +6,39 @@ import Auth from "./screens/Auth";
 import HomeScreen from "./screens/HomeScreen";
 import {Platform} from "react-native";
 import SplashScreen from "./screens/SplashScreen";
+import {StripeProvider} from "@stripe/stripe-react-native";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="SplashScreen">
-                {(Platform.OS !== 'web') &&
+        <StripeProvider
+            publishableKey="pk_test_51IjwEfC1js4lodAyBRNVJ83bNJtsel67h00mea3VzENDLODQNxDERGAa9hQ9h4yptXh7ZXAnju179KJLgy2HekGd0096Mtmzjr"
+            urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+            merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
+        >
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="SplashScreen">
+                    {(Platform.OS !== 'web') &&
+                        <Stack.Screen
+                            name="SplashScreen"
+                            component={SplashScreen as any}
+                            options={{headerShown: false}}/>
+                    }
                     <Stack.Screen
-                        name="SplashScreen"
-                        component={SplashScreen as any}
-                        options={{headerShown: false}}/>
-                }
-                <Stack.Screen
-                    name="Auth"
-                    component={Auth}
-                    options={{headerShown: false}}
-                />
-                <Stack.Screen
-                    name="App"
-                    component={HomeScreen}
-                    options={{headerShown: false}}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-        ;
+                        name="Auth"
+                        component={Auth}
+                        options={{headerShown: false}}
+                    />
+                    <Stack.Screen
+                        name="App"
+                        component={HomeScreen}
+                        options={{headerShown: false}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </StripeProvider>
+    );
 };
 
 export default App;
--- screens/components/HomeScreen/Settings.tsx
+++ screens/components/HomeScreen/Settings.tsx
@@ -1,10 +1,70 @@
-import {Button, View} from "react-native";
-import React from "react";
+import {Alert, Button, View} from "react-native";
+import React, {useEffect} from "react";
 import {googleSignOut} from "../../../utils/api";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
 import {RootStackParamList} from "../../../utils/types";
+import {useStripe} from "@stripe/stripe-react-native";
+import {URL} from "../../../utils/config";
 
 const Settings = ({navigation}: NativeStackScreenProps<RootStackParamList, 'Login'>) => {
+    const {initPaymentSheet, presentPaymentSheet} = useStripe();
+
+    const fetchPaymentSheetParams = async () => {
+        console.log("Fetching payment sheet params");
+        const response = await fetch(URL + '/create-payment-intent', {
+            method: 'POST',
+            body: JSON.stringify({'items': [{'id': "xl-tshirt"}]}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const {paymentIntent, ephemeralKey, customer} = await response.json();
+        await console.log(paymentIntent);
+        return {
+            paymentIntent,
+            ephemeralKey,
+            customer,
+        };
+    };
+
+    const initializePaymentSheet = async () => {
+        const {
+            paymentIntent,
+            ephemeralKey,
+            customer,
+
+        } = await fetchPaymentSheetParams();
+
+        const {error} = await initPaymentSheet({
+            merchantDisplayName: "Example, Inc.",
+            customerId: customer,
+            customerEphemeralKeySecret: ephemeralKey,
+            paymentIntentClientSecret: paymentIntent,
+            // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+            //methods that complete payment after a delay, like SEPA Debit and Sofort.
+            allowsDelayedPaymentMethods: true,
+            defaultBillingDetails: {
+                name: 'Jane Doe',
+            }
+        });
+        if (!error) {
+            setLoading(true);
+        }
+    };
+    useEffect(() => {
+        initializePaymentSheet();
+    }, []);
+
+    const openPaymentSheet = async () => {
+        console.log("Opening payment sheet");
+        const {error} = await presentPaymentSheet();
+
+        if (error) {
+            Alert.alert(`Error code: ${error.code}`, error.message);
+        } else {
+            Alert.alert('Success', 'Your order is confirmed!');
+        }
+    };
     return (
         <View>
             <Button title={"Logout"} onPress={() => {
@@ -12,6 +72,10 @@ const Settings = ({navigation}: NativeStackScreenProps<RootStackParamList, 'Logi
                         navigation.replace('Auth');
                     }
                 );
+            }}/>
+
+            <Button title={"Refill account"} onPress={() => {
+                openPaymentSheet();
             }}/>
         </View>
     )
